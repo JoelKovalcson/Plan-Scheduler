6432439146ca51fa70d10508d509e845ed469e52
Joel Kovalcson <joelkovalcson@gmail.com>
2021-11-13 17:25:38 -0500

    Textboxes change based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,33 +12,41 @@ const CURRENT_DATE = $("#currentDay");
 
 // Object to hold all the events
 var events;
-
+var cur_time = moment();
 
 function setupSchedule() {
 
     CURRENT_DATE.text(moment().format("dddd, MMMM Do"));
 
-    events.forEach(element => {
+    for(let i = 0; i < events.length; i++) {
         let row_el = $("<div>")
             .addClass("row")
-            .attr('id', element.hour);
+            .attr('id', events[i].hour);
 
         let hour_el = $("<div>")
             .addClass("col-1 hour")
             // Logic setup to handle any time range from 12AM - 11PM 
-            .text((element.hour < 12) ? ((element.hour != 0) ? element.hour + "AM" : "12AM") : (element.hour != 12) ? element.hour - 12 + "PM" : element.hour + "PM");
+            .text((events[i].hour < 12) ? ((events[i].hour != 0) ? events[i].hour + "AM" : "12AM") : (events[i].hour != 12) ? events[i].hour - 12 + "PM" : events[i].hour + "PM");
 
+        
         let event_el = $("<textarea>")
-            .addClass("col-10 past description")
-            .text(element.event);
-
+            .addClass("col-10 description")
+            .text(events[i].event);
+        if (events[i].hour < cur_time.hour()) {
+            event_el.addClass("past");
+        } else if (events[i].hour == cur_time.hour()) {
+            event_el.addClass("present");
+        } else {
+            event_el.addClass("future");
+        }
+        events[i].element = event_el;
         let save_el = $("<button>")
             .addClass("col-1 saveBtn");
 
         row_el.append(hour_el, event_el, save_el);
 
         SCHEDULE_CONTAINER.append(row_el);
-    });
+    }
 }
 
 // Sets events to a default of empty
@@ -60,7 +68,7 @@ function initEvents() {
     try {
         events = JSON.parse(localStorage.getItem("events"));
         if (events == null) events = defaultEvents();
-    } 
+    }
     // If localStorage is unable to be parsed, reset it.
     catch {
         events = defaultEvents();
@@ -68,4 +76,26 @@ function initEvents() {
     setupSchedule();
 }
 
+function checkTime() {
+    cur_time = moment();
+    events.forEach((event) => {
+        if (event.hour < cur_time.hour()) {
+            event.element.addClass("past");
+            event.element.removeClass("present");
+            event.element.removeClass("future");
+        } else if (event.hour == cur_time.hour()) {
+            event.element.addClass("present");
+            event.element.removeClass("past");
+            event.element.removeClass("future");
+        } else {
+            event.element.addClass("future");
+            event.element.removeClass("past");
+            event.element.removeClass("present");
+        }
+    });
+}
+
+// Load and set events
 initEvents();
+// Start timer to check time every minute
+setInterval(checkTime, 1000 * 60);
